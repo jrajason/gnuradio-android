0a138371ae8d1005a81c51be0d488e7354f63c72
jrajason <41758683+jrajason@users.noreply.github.com>
2023-11-09 23:27:45 -0500

    Update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-docker run -it --privileged -v /dev/bus/usb:/dev/bus/usb --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/home/android/.Xauthority:rw" gnuradio-android
+#docker build -t gnuradio-android .
+#docker run -it --privileged --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/home/android/.Xauthority:rw"  gnuradio-android
+
+#docker run -it --privileged -v /dev/bus/usb:/dev/bus/usb --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/home/android/.Xauthority:rw" gnuradio-android
