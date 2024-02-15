0884ced5a6691a8e9687dc26dbf8b7ed9b8a22bb
jrajason <41758683+jrajason@users.noreply.github.com>
2024-02-15 10:06:42 -0500

    Update build_system_setup.sh

--- build_system_setup.sh
+++ build_system_setup.sh
@@ -3,7 +3,7 @@
 ############### SYSTEM SPECIFIC DEFINES ############
 export USER_DIR=$USER
 export ANDROID_SDK_ROOT=$HOME/Android/Sdk
-export CMAKE=$HOME/cmake-3.23.2-linux-x86_64/bin/cmake
+export CMAKE=$HOME/cmake-3.18.1-linux-x86_64/bin/cmake
 export QT_VERSION_STRING=5.15.2
 export QT_HOME=$HOME/Qt
 export QT_INSTALL_PREFIX_NO_POSTFIX=${QT_HOME}/${QT_VERSION_STRING}/android
@@ -18,10 +18,10 @@ export BUILD_TYPE=Debug
 #export BUILD_TYPE=RelWithDebInfo
 export NDK_VERSION=23.1.7779620
 #export NDK_VERSION=21.3.6528147
-export API=28 # need ABI at least 28 for glob from my tests
+export API=29 # need ABI at least 28 for glob from my tests
 export JOBS=16
 export HOST_ARCH=linux-x86_64
-export ANDROID_SDK_BUILD_TOOLS=30.0.2
+export ANDROID_SDK_BUILD_TOOLS=31.0.0
 export ANDROID_NDK_ROOT=$ANDROID_SDK_ROOT/ndk/$NDK_VERSION
 export PYTHON_VERSION_MINOR=8
 
